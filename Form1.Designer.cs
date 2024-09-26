2c903335343ee9879bdbb56daf8fd0483247a7f2
Hevanafa <dhevansafa4@hotmail.com>
2024-09-26 12:23:20 +0700

    First upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace BitmapFontGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsbFontList = new System.Windows.Forms.ListBox();
+            this.lblFontName = new System.Windows.Forms.Label();
+            this.pbPreview = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudCharacterWidth = new System.Windows.Forms.NumericUpDown();
+            this.nudCharacterHeight = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudFontSize = new System.Windows.Forms.NumericUpDown();
+            this.txbSearch = new System.Windows.Forms.TextBox();
+            this.btnForeground = new System.Windows.Forms.Button();
+            this.btnBackground = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCharacterWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCharacterHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFontSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lsbFontList
+            // 
+            this.lsbFontList.FormattingEnabled = true;
+            this.lsbFontList.ItemHeight = 16;
+            this.lsbFontList.Location = new System.Drawing.Point(13, 42);
+            this.lsbFontList.Margin = new System.Windows.Forms.Padding(4);
+            this.lsbFontList.Name = "lsbFontList";
+            this.lsbFontList.Size = new System.Drawing.Size(191, 292);
+            this.lsbFontList.TabIndex = 0;
+            this.lsbFontList.SelectedIndexChanged += new System.EventHandler(this.lsbFontList_SelectedIndexChanged);
+            // 
+            // lblFontName
+            // 
+            this.lblFontName.AutoSize = true;
+            this.lblFontName.Location = new System.Drawing.Point(296, 9);
+            this.lblFontName.Name = "lblFontName";
+            this.lblFontName.Size = new System.Drawing.Size(87, 16);
+            this.lblFontName.TabIndex = 1;
+            this.lblFontName.Text = "Font name: []";
+            // 
+            // pbPreview
+            // 
+            this.pbPreview.Location = new System.Drawing.Point(299, 51);
+            this.pbPreview.Name = "pbPreview";
+            this.pbPreview.Size = new System.Drawing.Size(218, 255);
+            this.pbPreview.TabIndex = 2;
+            this.pbPreview.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(520, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Character width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(523, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Character height";
+            // 
+            // nudCharacterWidth
+            // 
+            this.nudCharacterWidth.Location = new System.Drawing.Point(523, 147);
+            this.nudCharacterWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudCharacterWidth.Name = "nudCharacterWidth";
+            this.nudCharacterWidth.Size = new System.Drawing.Size(120, 23);
+            this.nudCharacterWidth.TabIndex = 5;
+            this.nudCharacterWidth.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.nudCharacterWidth.ValueChanged += new System.EventHandler(this.nudCharacterWidth_ValueChanged);
+            // 
+            // nudCharacterHeight
+            // 
+            this.nudCharacterHeight.Location = new System.Drawing.Point(523, 192);
+            this.nudCharacterHeight.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudCharacterHeight.Name = "nudCharacterHeight";
+            this.nudCharacterHeight.Size = new System.Drawing.Size(120, 23);
+            this.nudCharacterHeight.TabIndex = 6;
+            this.nudCharacterHeight.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.nudCharacterHeight.ValueChanged += new System.EventHandler(this.nudCharacterHeight_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Preview";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Font size (em)";
+            // 
+            // nudFontSize
+            // 
+            this.nudFontSize.Location = new System.Drawing.Point(523, 70);
+            this.nudFontSize.Name = "nudFontSize";
+            this.nudFontSize.Size = new System.Drawing.Size(120, 23);
+            this.nudFontSize.TabIndex = 9;
+            this.nudFontSize.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.nudFontSize.ValueChanged += new System.EventHandler(this.nudFontSize_ValueChanged);
+            // 
+            // txbSearch
+            // 
+            this.txbSearch.Location = new System.Drawing.Point(12, 12);
+            this.txbSearch.Name = "txbSearch";
+            this.txbSearch.Size = new System.Drawing.Size(192, 23);
+            this.txbSearch.TabIndex = 10;
+            this.txbSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txbSearch_KeyDown);
+            // 
+            // btnForeground
+            // 
+            this.btnForeground.Location = new System.Drawing.Point(559, 237);
+            this.btnForeground.Name = "btnForeground";
+            this.btnForeground.Size = new System.Drawing.Size(40, 40);
+            this.btnForeground.TabIndex = 11;
+            this.btnForeground.UseVisualStyleBackColor = true;
+            this.btnForeground.Click += new System.EventHandler(this.btnForeground_Click);
+            // 
+            // btnBackground
+            // 
+            this.btnBackground.Location = new System.Drawing.Point(578, 256);
+            this.btnBackground.Name = "btnBackground";
+            this.btnBackground.Size = new System.Drawing.Size(40, 40);
+            this.btnBackground.TabIndex = 12;
+            this.btnBackground.UseVisualStyleBackColor = true;
+            this.btnBackground.Click += new System.EventHandler(this.btnBackground_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 366);
+            this.Controls.Add(this.btnForeground);
+            this.Controls.Add(this.btnBackground);
+            this.Controls.Add(this.txbSearch);
+            this.Controls.Add(this.nudFontSize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudCharacterHeight);
+            this.Controls.Add(this.nudCharacterWidth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pbPreview);
+            this.Controls.Add(this.lblFontName);
+            this.Controls.Add(this.lsbFontList);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCharacterWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCharacterHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFontSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lsbFontList;
+        private System.Windows.Forms.Label lblFontName;
+        private System.Windows.Forms.PictureBox pbPreview;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudCharacterWidth;
+        private System.Windows.Forms.NumericUpDown nudCharacterHeight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudFontSize;
+        private System.Windows.Forms.TextBox txbSearch;
+        private System.Windows.Forms.Button btnForeground;
+        private System.Windows.Forms.Button btnBackground;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitmapFontGenerator
+{
+    public partial class Form1 : Form
+    {
+        // Todo: upload to Github
+        // Todo: add the transparent background
+        // Todo: add more charsets
+
+        List<string> fontNames;
+        Dictionary<FontCacheKey, Bitmap> fontCache;
+        string top32 = " \u263a\u263B\u2665\u2666\u2663\u2660\u2022" + 
+            "\u25D8\u25CB\u25D9\u2642\u2640\u266A\u266B\u263C" +
+            "\u25BA\u25C4\u2195\u203C\u00B6\u00A7\u25AC\u21A8" +
+            "\u2191\u2193\u2192\u2190\u221F\u2194\u25B2\u25BC";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fontNames = new List<string>();
+            fontCache = new Dictionary<FontCacheKey, Bitmap>();
+            colourDialogue = new ColorDialog();
+
+            foreach (var font in FontFamily.Families)
+                fontNames.Add(font.Name);
+
+            lsbFontList.Items.AddRange(fontNames.ToArray());
+
+            Foreground = Color.Black;
+            Background = Color.White;
+        }
+
+        ColorDialog colourDialogue;
+
+        Color _foreground, _background;
+        Color Foreground {
+            get { return _foreground; }
+            set {
+                _foreground = value;
+                btnForeground.BackColor = value; } }
+
+        Color Background {
+            get { return _background; }
+            set {
+                _background = value;
+                btnBackground.BackColor = value; } }
+
+        //int CharacterWidth {
+        //    get { return (int)nudCharacterWidth.Value; }
+        //    set { nudCharacterWidth.Value = value;  }
+        //}
+
+        //int CharacterHeight {
+        //    get { return (int)nudCharacterHeight.Value; }
+        //    set { nudCharacterHeight.Value = value; }
+        //}
+
+        string getFontName() { return lsbFontList.SelectedItem.ToString(); }
+        int getFontSize() { return (int)nudFontSize.Value; }
+        int getCharacterWidth() { return (int)nudCharacterWidth.Value; }
+        int getCharacterHeight() { return (int)nudCharacterHeight.Value; }
+
+        void regeneratePreview() {
+            var fontName = getFontName();
+
+            // Todo: foreground & background
+            // Is caching really necessary?
+            var newKey = new FontCacheKey(fontName, getFontSize(), getCharacterWidth(), getCharacterHeight());
+
+            if (!fontCache.ContainsKey(newKey)) {
+                lblFontName.Text = fontName;
+
+                var bmp = new Bitmap(getCharacterWidth() * 16, getCharacterHeight() * 16);
+                fontCache.Add(newKey, bmp);
+
+                var font = new Font(fontName, (float)nudFontSize.Value);
+                var brush = new SolidBrush(Foreground);
+
+                using (Graphics g = Graphics.FromImage(bmp)) {
+                    g.Clear(Background);
+
+                    int index;
+
+                    for (var b = 0; b < 16; b++)
+                        for (var a = 0; a < 16; a++) {
+                            index = b * 16 + a;
+
+                            g.DrawString(
+                                index < 32
+                                ? "" + top32[index]
+                                : index == 0x7f
+                                ? "\u2302"
+                                : "" + ((char)index), font, brush, new Point(a * getCharacterWidth(), b * getCharacterHeight()));
+                        }
+                }
+
+                pbPreview.Image = bmp;
+            } else pbPreview.Image = fontCache[newKey];
+        }
+
+        private void lsbFontList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lsbFontList.SelectedIndex < 0) return;
+
+            regeneratePreview();
+        }
+
+        private void txbSearch_KeyDown(object sender, KeyEventArgs e) {
+            lsbFontList.Items.Clear();
+            lsbFontList.Items.AddRange(
+                fontNames
+                    .Select(x => x.ToLower())
+                    .Where(x => x.Contains(txbSearch.Text.ToLower())).ToArray());
+        }
+
+        private void nudFontSize_ValueChanged(object sender, EventArgs e) {
+            regeneratePreview();
+        }
+
+        private void nudCharacterWidth_ValueChanged(object sender, EventArgs e) {
+            regeneratePreview();
+        }
+
+        private void btnForeground_Click(object sender, EventArgs e) {
+            colourDialogue.Color = Foreground;
+            if (colourDialogue.ShowDialog() == DialogResult.OK) {
+                Foreground = colourDialogue.Color;
+                regeneratePreview();
+            }
+        }
+
+        private void btnBackground_Click(object sender, EventArgs e) {
+            colourDialogue.Color = Background;
+            if (colourDialogue.ShowDialog() == DialogResult.OK) {
+                Background = colourDialogue.Color;
+                regeneratePreview();
+            }
+        }
+
+        private void nudCharacterHeight_ValueChanged(object sender, EventArgs e) {
+            regeneratePreview();
+        }
+    }
+
+    // https://stackoverflow.com/questions/46023726/
+    class FontCacheKey {
+        static int currentID = 0;
+        static int getNextAvailableID() {
+            return currentID++;
+        }
+
+        public readonly int id;
+        public string Name;
+        public int fontSizeEm, characterWidth, characterHeight;
+        // Todo: foreground & background
+        public FontCacheKey(string fontName, int fontSizeEm, int characterWidth, int characterHeight) {
+            id = getNextAvailableID();
+            this.Name = fontName;
+            this.fontSizeEm = fontSizeEm;
+            this.characterWidth = characterWidth;
+            this.characterHeight = characterHeight;
+        }
+
+        public override int GetHashCode() {
+            return currentID;
+        }
+
+        public override bool Equals(object obj) {
+            return Equals(obj as FontCacheKey);
+        }
+
+        public bool Equals(FontCacheKey other) {
+            return other != null &&
+                other.Name == Name &&
+                other.fontSizeEm == fontSizeEm &&
+                other.characterWidth == characterWidth &&
+                other.characterHeight == characterHeight;
+        }
+    }
+}
